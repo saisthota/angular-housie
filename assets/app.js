62bca8c0feb76d5598272c31db63b1223e431f27
saisthota <me@saisthota.com>
2016-10-03 22:18:01 -0700

    Added HomeCtrl

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,20 @@
 /**
  * Created by saisthota on 10/3/16.
  */
+
+var app = angular.module('housieApp', ['ngRoute']);
+
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+}]);
+
+app.controller('HomeCtrl', function($scope) {
+
+    $scope.init = function() {
+        //Initialize 10x10 table here
+    }
+});
